c06c4c982dd4258a56af3523d375afdc0a41726d
AlejandraGalan <99278460+AlejandraGalan@users.noreply.github.com>
2022-05-20 14:21:08 +0200

    test tests_get_vaccine_date_tests.py working correctly

--- src/main/python/uc3m_care/vaccine_manager.py
+++ src/main/python/uc3m_care/vaccine_manager.py
@@ -1,7 +1,10 @@
 """Module """
-
+from datetime import datetime
 from uc3m_care.data.vaccine_patient_register import VaccinePatientRegister
 from uc3m_care.data.vaccination_appointment import VaccinationAppointment
+from uc3m_care.exception.vaccine_management_exception import VaccineManagementException
+
+
 
 class VaccineManager:
     """Class for providing the methods for managing the vaccination process"""
@@ -11,27 +14,27 @@ class VaccineManager:
         def __init__(self):
             pass
 
-        #pylint: disable=too-many-arguments
+        # pylint: disable=too-many-arguments
         # pylint: disable=no-self-use
-        def request_vaccination_id (self, patient_id,
-                                    name_surname,
-                                    registration_type,
-                                    phone_number,
-                                    age):
+        def request_vaccination_id(self, patient_id,
+                                   name_surname,
+                                   registration_type,
+                                   phone_number,
+                                   age):
             """Register the patinent into the patients file"""
             my_patient = VaccinePatientRegister(patient_id,
-                                                    name_surname,
-                                                    registration_type,
-                                                    phone_number,
-                                                    age)
+                                                name_surname,
+                                                registration_type,
+                                                phone_number,
+                                                age)
 
             my_patient.save_patient()
             return my_patient.patient_sys_id
 
-        def get_vaccine_date (self, input_file):
+        def get_vaccine_date(self, input_file, date:str):
             """Gets an appointment for a registered patient"""
-            my_sign= VaccinationAppointment.create_appointment_from_json_file(input_file)
-            #save the date in store_date.json
+            my_sign = VaccinationAppointment.create_appointment_from_json_file(input_file, date)
+            # save the date in store_date.json
             my_sign.save_appointment()
             return my_sign.date_signature
 
@@ -42,13 +45,13 @@ class VaccineManager:
 
     instance = None
 
-    def __new__ ( cls ):
+    def __new__(cls):
         if not VaccineManager.instance:
             VaccineManager.instance = VaccineManager.__VaccineManager()
         return VaccineManager.instance
 
-    def __getattr__ ( self, nombre ):
+    def __getattr__(self, nombre):
         return getattr(self.instance, nombre)
 
-    def __setattr__ ( self, nombre, valor ):
+    def __setattr__(self, nombre, valor):
         return setattr(self.instance, nombre, valor)
